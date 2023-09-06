dc9860eb480a8968010892e66bdecdf03592c87f
lama2923 <103530967+lama2923@users.noreply.github.com>
2023-09-06 23:36:34 +0300

    Add files via upload
    database update!

--- sayı tahmin.py
+++ sayı tahmin.py
@@ -9,6 +9,7 @@ import datetime
 import os
 import traceback
 import sys
+import sqlite3
 
 token = ''  # Buraya kendi kullanıcı tokeninizi koymanız gerekiyor
 channel_id = ''  # Buraya sayı tahmin oynayacağınız kanalın idsini koymanız gerekiyor
@@ -32,6 +33,41 @@ sys.stderr.close()
 sys.stderr = original_stderr
 
 
+db_connection_log = sqlite3.connect("Logs/log.db")
+db_cursor_log = db_connection_log.cursor()
+
+db_connection_extra = sqlite3.connect("Logs/extra.db")
+db_cursor_extra = db_connection_extra.cursor()
+
+
+db_cursor_log.execute('''
+    CREATE TABLE IF NOT EXISTS logs (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        log_time TEXT,
+        log_message TEXT
+    )
+''')
+db_connection_log.commit()
+
+db_cursor_extra.execute('''
+    CREATE TABLE IF NOT EXISTS extra_logs (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        log_time TEXT,
+        log_message TEXT
+    )
+''')
+db_connection_extra.commit()
+
+
+def log_to_database_log(log_time, log_message):
+    db_cursor_log.execute('INSERT INTO logs (log_time, log_message) VALUES (?, ?)',
+                          (log_time, log_message))
+    db_connection_log.commit()
+
+def log_to_database_extra(log_time, log_message):
+    db_cursor_extra.execute('INSERT INTO extra_logs (log_time, log_message) VALUES (?, ?)',
+                            (log_time, log_message))
+    db_connection_extra.commit()
 
 
 def ekran_temizle():
@@ -82,9 +118,13 @@ def create_crash_report(error_msg):
         pygame.mixer.music.play()
         while pygame.mixer.music.get_busy():
             pygame.time.Clock().tick(10)
+        zaman = datetime.datetime.now(tz).strftime("%Y-%m-%d   %H:%M:%S")
         with open("crash_reports.txt", "w", encoding="utf-8") as report_file:
-            zaman = datetime.datetime.now(tz).strftime("%Y-%m-%d   %H:%M:%S")
-            report_file.write(error_msg, zaman)
+            report_file.write(f"{error_msg}\n")
+            report_file.write(f"Tarih: {zaman}\n")
+            
+        
+        log_to_database_log(zaman, error_msg)
 
     except Exception as e:
         print("Crash raporu oluşturulurken bir hata oluştu:", str(e))
@@ -114,14 +154,16 @@ def bot():
                 zaman = datetime.datetime.now(tz).strftime("%Y-%m-%d   %H:%M:%S")
                 mesaj = f"Mesaj gönderildi: {count}"
                 print(f"{mesaj:<35}{zaman:>70}")
-                count += 1
                 
-                with open("log.txt", "a", encoding="utf-8") as dosya:
+                
+                with open("Logs/log.txt", "a", encoding="utf-8") as dosya:
                     dosya.write(f"{mesaj:<35}{zaman:>70}\n")
+                log_to_database_log(zaman, mesaj)
+                
             else:
                 zaman = datetime.datetime.now(tz).strftime("%Y-%m-%d   %H:%M:%S")
                 mesaj = f"Mesaj gönderilirken bir hata oluştu: {r.status_code}"
-                print(f"{mesaj:<50}{zaman:>70}")
+                print(f"{mesaj:<50}{zaman:>55}")
                 pygame.mixer.music.load("Sound/hata.wav")
                 pygame.mixer.music.play()
                 while pygame.mixer.music.get_busy():
@@ -144,23 +186,39 @@ def bot():
                 if messages:
                     last_message = messages[0]  
                     content = last_message['content']
+                    user_id = last_message['author']['id']
+                    username = last_message['author']['username']
 
                     if content.startswith(f'<@{kullanıcı_id}>, Tebrikler!'):  #burası ÇOK ÖNEMLİ buraya sayıyı bulunca botun size gönderdiği mesajın ilk 2-3 cümlesini eksiksiz koymanız lazım ve ayrıca sizin gözünüzle gördüğünüz kodda öyle yazılmıyor örnek : gözle görünen : @lama2923  kodda görünen: <@886616962678030427> 
-                        say_count = count - 1
-                        mesaj = f"Doğru sayı bulundu doğru sayı: {say_count}"
-                        print(f"{mesaj:<35}{zaman:>70}")
-                        count = 1  
                         
-                        with open("log.txt", "a", encoding="utf-8") as dosya:
+                        mesaj = f"Doğru sayı bulundu doğru sayı: {count}"
+                        print(f"{mesaj:<35}{zaman:>70}")
+                        count = 0  
+                        
+                        with open("Logs/log.txt", "a", encoding="utf-8") as dosya:
                             dosya.write(f"{mesaj:<35}{zaman:>70}\n")
+                        with open("Logs/extra.txt", "a", encoding="utf-8") as dosya:
+                            dosya.write(f"{username:<30}                {user_id}    {zaman:>70}\n")
+                        
+                        log_to_database_log(zaman, mesaj)
+                        extra_log_message = f"Bot Adı: {username} | Bot ID'si: {user_id}"
+                        log_to_database_extra(zaman, extra_log_message)
+                        log_to_database_extra(zaman, extra_log_message)
                         
                         pygame.mixer.music.load("Sound/buldum.wav")
                         pygame.mixer.music.play()
                         while pygame.mixer.music.get_busy():
                             pygame.time.Clock().tick(10)
+                        count += 1
                         pass
                     elif content.startswith(f'<@{kullanıcı_id}>, Yazılan'): #burası ÇOK ÖNEMLİ buraya sayıyı bulunamayınca botun size gönderdiği mesajın ilk 2-3 cümlesini eksiksiz koymanız lazım ve ayrıca sizin gözünüzle gördüğünüz kodda öyle yazılmıyor örnek : gözle görünen : @lama2923  kodda görünen: <@886616962678030427> 
+                        with open("Logs/extra.txt", "a", encoding="utf-8") as dosya:
+                            dosya.write(f"{username:<30}                {user_id}    {zaman:>70}\n")
+                        extra_log_message = f"Bot Adı: {username} | Bot ID'si: {user_id}"
+                        log_to_database_extra(zaman, extra_log_message)
+                        count += 1
                         pass
+
                     else:
                         time.sleep(3)
                         while True:
@@ -184,7 +242,7 @@ def bot():
                                     zaman = datetime.datetime.now(tz).strftime("%Y-%m-%d   %H:%M:%S")
                                     mesaj_tekrar = f"Tekrar mesajı gönderildi: {wcount}"
                                     print(f"{mesaj_tekrar:<35}{zaman:>70}")
-                                    with open("log.txt", "a", encoding="utf-8") as dosya:
+                                    with open("Logs/log.txt", "a", encoding="utf-8") as dosya:
                                         dosya.write(f"{mesaj_tekrar:<35}{zaman:>70}\n")
                                 else:
                                     mesaj = f"Mesaj gönderilirken bir hata oluştu: {r.status_code}"
@@ -208,6 +266,8 @@ def bot():
                                     if messages:
                                         last_message = messages[0]  
                                         content = last_message['content']
+                                        user_id = last_message['author']['id']
+                                        username = last_message['author']['username']
 
                                         if content.startswith(f'<@{kullanıcı_id}>, Tebrikler!'):  #burası ÇOK ÖNEMLİ buraya sayıyı bulunca botun size gönderdiği mesajın ilk 2-3 cümlesini eksiksiz koymanız lazım ve ayrıca sizin gözünüzle gördüğünüz kodda öyle yazılmıyor örnek : gözle görünen : @lama2923  kodda görünen: <@886616962678030427> 
                                             wcount
@@ -215,8 +275,10 @@ def bot():
                                             print(f"{mesaj:<35}{zaman:>70}")
                                             count = 1  
                             
-                                            with open("log.txt", "a", encoding="utf-8") as dosya:
+                                            with open("Logs/log.txt", "a", encoding="utf-8") as dosya:
                                                 dosya.write(f"{mesaj:<35}{zaman:>70}\n")
+                                            with open("Logs/extra.txt", "a", encoding="utf-8") as dosya:
+                                                dosya.write(f"{username:<30}                {user_id}    {zaman:>70}\n")
                             
                                                 pygame.mixer.music.load("Sound/buldum.wav")
                                                 pygame.mixer.music.play()
@@ -224,7 +286,13 @@ def bot():
                                                     pygame.time.Clock().tick(10)
                                                 break
                                         elif content.startswith(f'<@{kullanıcı_id}>, Yazılan'): #burası ÇOK ÖNEMLİ buraya sayıyı bulunamayınca botun size gönderdiği mesajın ilk 2-3 cümlesini eksiksiz koymanız lazım ve ayrıca sizin gözünüzle gördüğünüz kodda öyle yazılmıyor örnek : gözle görünen : @lama2923  kodda görünen: <@886616962678030427> 
+                                            with open("Logs/extra.txt", "a", encoding="utf-8") as dosya:
+                                                dosya.write(f"{username:<30}                {user_id}    {zaman:>70}\n")
                                             break
+                                        else:
+                                            with open("Logs/extra.txt", "a", encoding="utf-8") as dosya:
+                                                dosya.write(f"Bot yanıt vermiyor yada belirlediğiniz bot yanıtları yanlış ayarlanmış.{zaman:>70}\n")
+
                                     else:
                                         print("Kanalda henüz mesaj yok.")
                                 else:
@@ -249,7 +317,14 @@ def bot():
 
         time.sleep(1.8)  
 
-if not os.path.exists("log.txt"):
-    open("log.txt", "w").close()  
+if not os.path.exists("Logs/log.txt"):
+    open("Logs/log.txt", "w").close()  
 
 bot()
+
+
+db_cursor_log.close()
+db_connection_log.close()
+
+db_cursor_extra.close()
+db_connection_extra.close()
