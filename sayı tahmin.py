7a6110f325bdce3ba1c81059e9cbdaaa1b974acf
lama2923 <103530967+lama2923@users.noreply.github.com>
2023-09-07 17:29:49 +0300

    Add files via upload
    fix

--- sayı tahmin.py
+++ sayı tahmin.py
@@ -1,6 +1,5 @@
+
 #BU MESAJI OKUYUN. KOYDUĞUM BÜTÜN YORUM SATIRLARINI OKUYUN ÇÜNKÜ BİRİNDE BİLE HATA YAPARSANIZ KOD ÇALIŞMAZ HEPSİNİ EKSİKSİZ OKUYUN VE EKSİKSİZ DOLDURUN.(BÜTÜN KODU İNCELEYİN def bot() kısmını bile)
-
-
 
 import time
 import requests
@@ -8,13 +7,15 @@ import pytz
 import datetime
 import os
 import traceback
-import sys
 import sqlite3
+import tkinter as tk
+import threading
+import multiprocessing
+import sys  
 
 token = ''  # Buraya kendi kullanıcı tokeninizi koymanız gerekiyor
 channel_id = ''  # Buraya sayı tahmin oynayacağınız kanalın idsini koymanız gerekiyor
 kullanıcı_id = ''  # Buraya kendi kullanıcı id nizi koymanız gerekiyor
-
 
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 
@@ -32,9 +33,9 @@ pygame.init()
 sys.stderr.close()
 sys.stderr = original_stderr
 
-if not os.path.exists("Logs"):
-    os.mkdir("Logs")
+tz = pytz.timezone('Europe/Istanbul')
 
+pygame.init()
 
 db_connection_log = sqlite3.connect("Logs/log.db")
 db_cursor_log = db_connection_log.cursor()
@@ -42,12 +43,13 @@ db_cursor_log = db_connection_log.cursor()
 db_connection_extra = sqlite3.connect("Logs/extra.db")
 db_cursor_extra = db_connection_extra.cursor()
 
+quit_flag = False
 
 db_cursor_log.execute('''
     CREATE TABLE IF NOT EXISTS logs (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        log_time TEXT,
-        log_message TEXT
+        log_message TEXT,
+        log_time TEXT
     )
 ''')
 db_connection_log.commit()
@@ -55,65 +57,39 @@ db_connection_log.commit()
 db_cursor_extra.execute('''
     CREATE TABLE IF NOT EXISTS extra_logs (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        log_time TEXT,
-        log_message TEXT
+        bot_name TEXT,
+        bot_id TEXT,
+        log_time TEXT
     )
 ''')
 db_connection_extra.commit()
 
 
 def log_to_database_log(log_time, log_message):
-    db_cursor_log.execute('INSERT INTO logs (log_time, log_message) VALUES (?, ?)',
-                          (log_time, log_message))
+    db_cursor_log.execute('INSERT INTO logs (log_message, log_time) VALUES (?, ?)',
+                          (log_message, log_time))
     db_connection_log.commit()
 
-def log_to_database_extra(log_time, log_message):
-    db_cursor_extra.execute('INSERT INTO extra_logs (log_time, log_message) VALUES (?, ?)',
-                            (log_time, log_message))
+
+def log_to_database_extra(bot_name, bot_id, log_time):
+    db_cursor_extra.execute('INSERT INTO extra_logs (bot_name, bot_id, log_time) VALUES (?, ?, ?)',
+                            (bot_name, bot_id, log_time))
     db_connection_extra.commit()
 
 
-def ekran_temizle():
-    os.system('cls' if os.name == 'nt' else 'clear')
-
-internet_print = "internet bağlantınız olmadığı için program düzgün çalışamıyor"
-
-internet = True
-
-def check_internet_connection():
-    global internet ,internet_print
+def internet_available():
     try:
         requests.get("https://www.google.com", timeout=5)
-        internet = True
         return True
     except requests.ConnectionError:
-        internet = False
         return False
 
-def internet_giriş_yasak():
-    global internet
-    time.sleep(0.5)
-    print(internet_print)
-    check_internet_connection()
-    if not internet:
-        internet_giriş_yasak()
-    else:
-        ekran_temizle()
 
-while True:
-    check_internet_connection()
-    if not internet:
-        internet_giriş_yasak()
-    else:
-        break
+def check_internet_connection():
+    while not internet_available():
+        print("Internet bağlantısı yok.")
+        time.sleep(0.5)
 
-def internet_kontrol():
-    global internet
-    check_internet_connection()
-    if not internet:
-        internet_giriş_yasak()
-    else:
-        pass
 
 def create_crash_report(error_msg):
     try:
@@ -125,44 +101,60 @@ def create_crash_report(error_msg):
         with open("crash_reports.txt", "w", encoding="utf-8") as report_file:
             report_file.write(f"{error_msg}\n")
             report_file.write(f"Tarih: {zaman}\n")
-            
-        
+
         log_to_database_log(zaman, error_msg)
 
     except Exception as e:
         print("Crash raporu oluşturulurken bir hata oluştu:", str(e))
+quit_event = multiprocessing.Event()
+quit_flag = multiprocessing.Value('b', False)
+quit_flag_lock = multiprocessing.Lock()
 
-def bot():
+
+
+def bot_thread(quit_flag):
     global token, channel_id, kullanıcı_id
     count = 1
 
-    input("Enter tuşuna basarak botu başlatın...")
+    pygame.init()  # Pygame'i başlat
+
+    db_connection_log = sqlite3.connect("Logs/log.db")
+    db_cursor_log = db_connection_log.cursor()
+
+    db_connection_extra = sqlite3.connect("Logs/extra.db")
+    db_cursor_extra = db_connection_extra.cursor()
     pygame.mixer.init()
     while True:
-        internet_kontrol()
-        
+        with quit_flag_lock:
+            if quit_flag.value:
+                pygame.quit()
+                break
+
+        check_internet_connection()
+
         if count > 100:
             count = 1
         payload = {
             'content': str(count)
         }
-        
+
         headers = {
-            'Authorization': f'{token}'  
+            'Authorization': f'{token}'
         }
 
         try:
-            r = requests.post(f'https://discord.com/api/v9/channels/{channel_id}/messages', data=payload, headers=headers)
+            r = requests.post(f'https://discord.com/api/v9/channels/{channel_id}/messages', data=payload,
+                              headers=headers)
             if r.status_code == 200:
                 zaman = datetime.datetime.now(tz).strftime("%Y-%m-%d   %H:%M:%S")
                 mesaj = f"Mesaj gönderildi: {count}"
                 print(f"{mesaj:<35}{zaman:>70}")
-                
-                
+
                 with open("Logs/log.txt", "a", encoding="utf-8") as dosya:
                     dosya.write(f"{mesaj:<35}{zaman:>70}\n")
+
                 log_to_database_log(zaman, mesaj)
-                
+
             else:
                 zaman = datetime.datetime.now(tz).strftime("%Y-%m-%d   %H:%M:%S")
                 mesaj = f"Mesaj gönderilirken bir hata oluştu: {r.status_code}"
@@ -171,43 +163,39 @@ def bot():
                 pygame.mixer.music.play()
                 while pygame.mixer.music.get_busy():
                     pygame.time.Clock().tick(10)
-                
-        except Exception as e:
 
+        except Exception as e:
             error_msg = traceback.format_exc()
             create_crash_report(error_msg)
-
             print("Bir hata oluştu, crash raporu oluşturuldu.")
             break
 
-        time.sleep(1)  
+        time.sleep(1)
 
         try:
             r = requests.get(f'https://discord.com/api/v9/channels/{channel_id}/messages', headers=headers)
             if r.status_code == 200:
                 messages = r.json()
                 if messages:
-                    last_message = messages[0]  
+                    last_message = messages[0]
                     content = last_message['content']
                     user_id = last_message['author']['id']
                     username = last_message['author']['username']
 
-                    if content.startswith(f'<@{kullanıcı_id}>, Tebrikler!'):  #burası ÇOK ÖNEMLİ buraya sayıyı bulunca botun size gönderdiği mesajın ilk 2-3 cümlesini eksiksiz koymanız lazım ve ayrıca sizin gözünüzle gördüğünüz kodda öyle yazılmıyor örnek : gözle görünen : @lama2923  kodda görünen: <@886616962678030427> 
-                        
+                    if content.startswith(f'<@{kullanıcı_id}>, Tebrikler!'): #burası ÇOK ÖNEMLİ buraya sayıyı bulunca botun size gönderdiği mesajın ilk 2-3 cümlesini eksiksiz koymanız lazım ve ayrıca sizin gözünüzle gördüğünüz kodda öyle yazılmıyor örnek : gözle görünen : @lama2923  kodda görünen: <@886616962678030427> 
                         mesaj = f"Doğru sayı bulundu doğru sayı: {count}"
                         print(f"{mesaj:<35}{zaman:>70}")
-                        count = 0  
-                        
+                        count = 0
+
                         with open("Logs/log.txt", "a", encoding="utf-8") as dosya:
                             dosya.write(f"{mesaj:<35}{zaman:>70}\n")
                         with open("Logs/extra.txt", "a", encoding="utf-8") as dosya:
                             dosya.write(f"{username:<30}                {user_id}    {zaman:>70}\n")
-                        
+
+                        log_to_database_extra(username, user_id, zaman)
                         log_to_database_log(zaman, mesaj)
-                        extra_log_message = f"Bot Adı: {username} | Bot ID'si: {user_id}"
-                        log_to_database_extra(zaman, extra_log_message)
-                        log_to_database_extra(zaman, extra_log_message)
-                        
+                        log_to_database_extra(username, user_id, zaman)
+
                         pygame.mixer.music.load("Sound/buldum.wav")
                         pygame.mixer.music.play()
                         while pygame.mixer.music.get_busy():
@@ -217,36 +205,41 @@ def bot():
                     elif content.startswith(f'<@{kullanıcı_id}>, Yazılan'): #burası ÇOK ÖNEMLİ buraya sayıyı bulunamayınca botun size gönderdiği mesajın ilk 2-3 cümlesini eksiksiz koymanız lazım ve ayrıca sizin gözünüzle gördüğünüz kodda öyle yazılmıyor örnek : gözle görünen : @lama2923  kodda görünen: <@886616962678030427> 
                         with open("Logs/extra.txt", "a", encoding="utf-8") as dosya:
                             dosya.write(f"{username:<30}                {user_id}    {zaman:>70}\n")
-                        extra_log_message = f"Bot Adı: {username} | Bot ID'si: {user_id}"
-                        log_to_database_extra(zaman, extra_log_message)
+
+                        log_to_database_extra(username, user_id, zaman)
+
                         count += 1
                         pass
 
                     else:
+                        log_to_database_extra(username, user_id, zaman)
                         time.sleep(3)
                         while True:
-                            internet_kontrol()
-                            
+                            check_internet_connection()
+
                             if count > 100:
                                 count = 1
-                            wcount = count -1
-                            
+                            wcount = count
+
                             payload = {
                                 'content': str(wcount)
                             }
                             headers = {
-                                'Authorization': f'{token}'  
+                                'Authorization': f'{token}'
                             }
-                            
-                            try:
-                                r = requests.post(f'https://discord.com/api/v9/channels/{channel_id}/messages', data=payload, headers=headers)
 
-                                if r.status_code == 200:    
+                            try:
+                                r = requests.post(f'https://discord.com/api/v9/channels/{channel_id}/messages',
+                                                  data=payload, headers=headers)
+
+                                if r.status_code == 200:
                                     zaman = datetime.datetime.now(tz).strftime("%Y-%m-%d   %H:%M:%S")
                                     mesaj_tekrar = f"Tekrar mesajı gönderildi: {wcount}"
                                     print(f"{mesaj_tekrar:<35}{zaman:>70}")
                                     with open("Logs/log.txt", "a", encoding="utf-8") as dosya:
                                         dosya.write(f"{mesaj_tekrar:<35}{zaman:>70}\n")
+                                    log_to_database_log(zaman, mesaj_tekrar)
+
                                 else:
                                     mesaj = f"Mesaj gönderilirken bir hata oluştu: {r.status_code}"
                                     print(f"{mesaj:<50}{zaman:>70}")
@@ -260,29 +253,35 @@ def bot():
                                 print("Bir hata oluştu, crash raporu oluşturuldu.")
                                 break
 
-                            time.sleep(1)  
+                            time.sleep(1)
 
                             try:
-                                r = requests.get(f'https://discord.com/api/v9/channels/{channel_id}/messages', headers=headers)
+                                r = requests.get(f'https://discord.com/api/v9/channels/{channel_id}/messages',
+                                                 headers=headers)
                                 if r.status_code == 200:
                                     messages = r.json()
                                     if messages:
-                                        last_message = messages[0]  
+                                        last_message = messages[0]
                                         content = last_message['content']
                                         user_id = last_message['author']['id']
                                         username = last_message['author']['username']
 
-                                        if content.startswith(f'<@{kullanıcı_id}>, Tebrikler!'):  #burası ÇOK ÖNEMLİ buraya sayıyı bulunca botun size gönderdiği mesajın ilk 2-3 cümlesini eksiksiz koymanız lazım ve ayrıca sizin gözünüzle gördüğünüz kodda öyle yazılmıyor örnek : gözle görünen : @lama2923  kodda görünen: <@886616962678030427> 
+                                        if content.startswith(f'<@{kullanıcı_id}>, Tebrikler!'): #burası ÇOK ÖNEMLİ buraya sayıyı bulunca botun size gönderdiği mesajın ilk 2-3 cümlesini eksiksiz koymanız lazım ve ayrıca sizin gözünüzle gördüğünüz kodda öyle yazılmıyor örnek : gözle görünen : @lama2923  kodda görünen: <@886616962678030427> 
                                             wcount
                                             mesaj = f"Doğru sayı bulundu doğru sayı: {wcount}"
                                             print(f"{mesaj:<35}{zaman:>70}")
-                                            count = 1  
-                            
+                                            count = 1
+
                                             with open("Logs/log.txt", "a", encoding="utf-8") as dosya:
                                                 dosya.write(f"{mesaj:<35}{zaman:>70}\n")
+
+                                                log_to_database_extra(username, user_id, zaman)
+                                                log_to_database_log(zaman, mesaj)
                                             with open("Logs/extra.txt", "a", encoding="utf-8") as dosya:
                                                 dosya.write(f"{username:<30}                {user_id}    {zaman:>70}\n")
-                            
+
+                                                log_to_database_extra(username, user_id, zaman)
+
                                                 pygame.mixer.music.load("Sound/buldum.wav")
                                                 pygame.mixer.music.play()
                                                 while pygame.mixer.music.get_busy():
@@ -291,10 +290,17 @@ def bot():
                                         elif content.startswith(f'<@{kullanıcı_id}>, Yazılan'): #burası ÇOK ÖNEMLİ buraya sayıyı bulunamayınca botun size gönderdiği mesajın ilk 2-3 cümlesini eksiksiz koymanız lazım ve ayrıca sizin gözünüzle gördüğünüz kodda öyle yazılmıyor örnek : gözle görünen : @lama2923  kodda görünen: <@886616962678030427> 
                                             with open("Logs/extra.txt", "a", encoding="utf-8") as dosya:
                                                 dosya.write(f"{username:<30}                {user_id}    {zaman:>70}\n")
+
+                                                log_to_database_extra(username, user_id, zaman)
                                             break
                                         else:
                                             with open("Logs/extra.txt", "a", encoding="utf-8") as dosya:
-                                                dosya.write(f"Bot yanıt vermiyor yada belirlediğiniz bot yanıtları yanlış ayarlanmış.{zaman:>70}\n")
+                                                dosya.write(
+                                                    f"Bot yanıt vermiyor yada belirlediğiniz bot yanıtları yanlış ayarlanmış.{zaman:>70}\n")
+                                                username = "Null"
+                                                user_id = "Null"
+
+                                                log_to_database_extra(username, user_id, zaman)
 
                                     else:
                                         print("Kanalda henüz mesaj yok.")
@@ -306,8 +312,8 @@ def bot():
                                 print("Bir hata oluştu, crash raporu oluşturuldu.")
                                 break
 
-                            time.sleep(2.5)  
-                        
+                            time.sleep(2.5)
+
                 else:
                     print("Kanalda henüz mesaj yok.")
             else:
@@ -318,16 +324,62 @@ def bot():
             print("Bir hata oluştu, crash raporu oluşturuldu.")
             break
 
-        time.sleep(1.8)  
-
+        time.sleep(1.8)
+quit_event = multiprocessing.Event()
 if not os.path.exists("Logs/log.txt"):
-    open("Logs/log.txt", "w").close()  
+    open("Logs/log.txt", "w").close()
+quit_flag_lock = threading.Lock()
+def quit_application(root, quit_flag):
+    quit_flag.value = True
+    root.quit()
 
-bot()
+def tkinter_loop(quit_flag):
+    global quit_event  
+    root = tk.Tk()
+    root.title("Tkinter Penceresi Başlığı")
+
+    quit_button = tk.Button(root, text="Çıkış", command=lambda: quit_application(root, quit_flag))
+    quit_button.pack()
+
+    while not quit_flag.value:
+        try:
+            root.update()  
+            time.sleep(0.1)
+        except tk.TclError:
+            break
 
 
-db_cursor_log.close()
-db_connection_log.close()
 
-db_cursor_extra.close()
-db_connection_extra.close()
+def create_gui():
+    root = tk.Tk()
+    root.title("Quit Screen")
+    root.geometry("500x300")
+
+    
+    root.configure(bg="#36393F")
+    root.option_add("*TButton*highlightBackground", "#7289DA")
+    root.option_add("*TButton*highlightColor", "#7289DA")
+    root.option_add("*TButton*background", "#7289DA")
+    root.option_add("*TButton*foreground", "white")
+    root.option_add("*TButton.font", ("Arial", 14))  
+
+    
+    label = tk.Label(root, text="Programı çıkış butonuna basarak kapatmanız.\nLoglarken sorun olmaması için daha sağlıklı olacaktır.", bg="#36393F", fg="white", font=("Arial", 16))
+    label.pack(pady=20)
+
+    
+    button = tk.Button(root, text="Çıkış", command=lambda: quit_application(root, quit_flag), relief=tk.RAISED, width=20, height=2, font=("Arial", 14))
+    button.pack()
+
+    
+    root.mainloop()
+if __name__ == "__main__":
+    quit_flag = multiprocessing.Value('b', False)
+    bot_process = multiprocessing.Process(target=bot_thread, args=(quit_flag,))
+    tkinter_thread = threading.Thread(target=create_gui)
+
+    bot_process.start()
+    tkinter_thread.start()
+
+    bot_process.join()
+    tkinter_thread.join()
